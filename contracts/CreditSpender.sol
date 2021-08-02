9c067961d8012ff173b2c87fb3ea9549f0bb2aa3
George <gbh218@gmail.com>
2021-08-02 10:26:35 -0400

    Organized folder structure

--- contracts/CreditSpender.sol
+++ contracts/CreditSpender.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-import "../interfaces/IERC20.sol";
+import "./interfaces/IERC20.sol";
 
 contract CreditSpenderFactory {
     
--- contracts/SavingsPool.sol
+++ contracts/SavingsPool.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-import "../interfaces/IERC20.sol";
-import "../interfaces/ILendingPool.sol";
-import "../interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IERC20.sol";
+import "./interfaces/ILendingPool.sol";
+import "./interfaces/IUniswapV2Router02.sol";
 
 contract SavingsPool {
   mapping(address => bool) private isMember;
